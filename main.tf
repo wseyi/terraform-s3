900f24880c458816ba3b57024765857deb719dbc
seyi williams <seyiwilliams12@gmail.com>
2024-03-13 13:07:51 +0100

    added pemission setting to allow only root user delete files in this s3 bucket

--- main.tf
+++ main.tf
@@ -7,3 +7,26 @@ resource "aws_s3_bucket" "assignment" {
     Environment = var.Environment
   }
 }
+
+resource "aws_iam_policy" "root_delete_policy" {
+  name        = "root_delete_policy"
+  description = "Allows only root user to delete files in S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Action    = [
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion",
+        "s3:DeleteObjectTagging",
+        "s3:DeleteObjectVersionTagging"
+      ],
+      Resource  = aws_s3_bucket.tf-bucket_bucket.arn,
+      Condition = {
+        StringEquals = {
+          "aws:userid" = "your_root_user_arn"
+        }
+      }
+    }]
+  })
